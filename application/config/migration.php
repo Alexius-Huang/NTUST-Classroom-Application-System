869de7b7d893fc722a68aa723c3b90ba4d63d70b
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-04-11 21:28:07 +0800

    Add device_apply and device_log table

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20170408050400;
+$config['migration_version'] = 20170411211700;
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/migrations/20170411211200_add_device_apply_table.php
@@ -0,0 +1,62 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed!');
+
+class Migration_Add_device_apply_table extends CI_Migration {
+
+  function up() {
+    $this->dbforge->add_field(array(
+      'id' => array(
+        'type'           => 'INT',
+        'constraint'     => 11,
+        'unsigned'       => TRUE,
+        'auto_increment' => TRUE
+      ),
+      'student_id' => array(
+        'type' => 'VARCHAR',
+        'constraint' => 16
+      ),
+      'status' => array(
+        'type' => 'TINYINT',
+        'constraint' => 1,
+        'unsigned' => TRUE,
+        'default' => '0'
+      ),
+      'date' => array(
+        'type' => 'DATE'
+      ),
+      'organization' => array(
+        'type' => 'TEXT',
+      ),
+      'applicant' => array(
+        'type' => 'TEXT'
+      ),
+      'phone' => array(
+        'type' => 'TEXT'
+      ),
+      'purpose' => array(
+        'type' => 'TEXT'
+      ),
+      'ip' => array(
+        'type' => 'TEXT'
+      ),
+      'created_at' => array(
+        'type' => 'BIGINT',
+        'constraint' => 20,
+        'unsigned' => TRUE
+      ),
+      'updated_at' => array(
+        'type' => 'BIGINT',
+        'constraint' => 20,
+        'unsigned' => TRUE
+      )
+    ));
+    $this->dbforge->add_key('id', TRUE);
+    $this->dbforge->create_table('DeviceApply', TRUE);
+    echo '<p>Migration Created : 20170411211200_add_device_apply_table</p>';
+  }
+
+  function down() {
+    $this->dbforge->drop_table('DeviceApply');
+    echo '<p>Migration Dropped : 20170411211200_add_device_apply_table</p>';
+  }
+
+}
