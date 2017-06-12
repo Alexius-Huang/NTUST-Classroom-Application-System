aaac2b4294d89646dc3e3df5b4d00eef9968ea2a
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-06-12 13:15:53 +0800

    Revise devise pdf view

--- application/views/main/device/device_pdf_view.php
+++ application/views/main/device/device_pdf_view.php
@@ -13,33 +13,78 @@ pdfMake.fonts = {
     normal: "kaiu.ttf"
   }
 };
+var column_a = [];
+column_a.push({ text: '條目', style: 'tableHeader', alignment: 'center', margin: [5, 5]});
+column_a.push({ text: '內容', style: 'tableHeader', alignment: 'center', margin: [5, 5]});
+
+var body_a = [column_a];
+body_a.push([{ text: '借用日期', alignment: 'center', margin: [8, 5] }, { text: "<?php echo $device_apply['date']; ?>", margin: [8, 5] }]);
+body_a.push([{ text: '歸還日期', alignment: 'center', margin: [8, 5] }, { text: "<?php echo $device_apply['end_date']; ?>", margin: [8, 5] }]);
+body_a.push([{ text: '借用單位', alignment: 'center', margin: [8, 5] }, { text: "<?php echo $device_apply['organization']; ?>", margin: [8, 5] }]);
+body_a.push([{ text: '申請人員', alignment: 'center', margin: [8, 5] }, { text: "<?php echo $device_apply['applicant']; ?>", margin: [8, 5] }]);
+body_a.push([{ text: '聯絡電話', alignment: 'center', margin: [8, 5] }, { text: "<?php echo $device_apply['phone']; ?>", margin: [8, 5] }]);
+body_a.push([{ text: '借用目的及地點', alignment: 'center', margin: [8, 5] }, { text: "<?php echo $device_apply['purpose']; ?>", margin: [8, 5] }]);
+
+var table_a = {
+  table: {
+    widths: [ '*', '*'],
+    headerRows: 1,
+    body: body_a
+  },
+}
+
+var column_b = [];
+column_b.push({ text: '器材名稱', style: 'tableHeader', alignment: 'center', margin: [5, 5]});
+column_b.push({ text: '借用數量', style: 'tableHeader', alignment: 'center', margin: [5, 5]});
+column_b.push({ text: '編號', style: 'tableHeader', alignment: 'center', margin: [5, 5]});
+
+var body_b = [column_b];
+<?php foreach($devices as $index => $device): ?>
+  body_b.push([
+    { text: '<?php echo $device["name_zh-TW"]."(".$device["name_en-us"].")" ?>', alignment: 'center', margin: [8, 5] },
+    { text: '<?php echo $device["lease_count"] ?>', alignment: 'center', margin: [8, 5] },
+    { text: '', margin: [8, 5] }
+  ]);
+<?php endforeach; ?>
+
+var table_b = {
+  table: {
+    widths: ['*', 'auto', 'auto'],
+    headerRows: 1,
+    body: body_b
+  }
+};
+
+var column_c = [];
+column_c.push({ text: '條目', style: 'tableHeader', alignment: 'center', margin: [5, 5]});
+column_c.push({ text: '內容', style: 'tableHeader', alignment: 'center', margin: [5, 5]});
+
+var body_c = [column_c];
+body_c.push([{ text: '出借日期', alignment: 'center', margin: [8, 5] }, { text: '      月      日', alignment: 'center', margin: [8, 5] }]);
+body_c.push([{ text: '出借人員簽名', alignment: 'center', margin: [8, 5] }, { text: '', margin: [8, 5] }]);
+body_c.push([{ text: '實際歸還日期', alignment: 'center', margin: [8, 5] }, { text: '      月      日', alignment: 'center', margin: [8, 5] }]);
+body_c.push([{ text: '收件人員簽名', alignment: 'center', margin: [8, 5] }, { text: '', margin: [8, 5] }]);
+
+var table_c = {
+  table: {
+    widths: ['*', '*'],
+    headerRows: 1,
+    body: body_c
+  }
+}
 
 var docDefinition = {
   content: [
     { text: "器材借用提領單", fontSize: 20, margin: [10, 3], alignment: 'center' },
     { text: "Device Leasing Check List", fontSize: 16, margin: [5, 10], alignment: 'center' },
-    { text: '【 借用申請資訊 Application Info 】', fontSize: 16, margin: [10, 5] },
-    { text: "借用日期：<?php echo $device_apply['date']; ?>", fontSize: 14, margin: [10, 5] },
-    { text: "預計歸還日期：<?php echo $device_apply['end_date']; ?>", fontSize: 14, margin: [10, 5] },
-    { text: "借用單位：<?php echo $device_apply['organization']; ?>", fontSize: 14, margin: [10, 5] },
-    { text: "借用申請人：<?php echo $device_apply['applicant']; ?> (<?php echo $device_apply['applicantPosition']; ?>)", fontSize: 14, margin: [10, 5] },
-    { text: "聯絡電話：<?php echo $device_apply['phone']; ?>", fontSize: 14, margin: [10, 5] },
-    { text: "借用目的：<?php echo $device_apply['purpose']; ?>", fontSize: 14, margin: [10, 5] },
+    { text: '・借用申請資訊', fontSize: 14, margin: [10, 5] },
+    table_a,
     { text: "", margin: [10, 3] },
-    { text: '【 借用核對資訊 Check Info 】', fontSize: 16, margin: [10, 5] },
-    { text: "出借日期：       月    日", fontSize: 14, margin: [10, 5]},
-    { text: "出借人員簽名：           ", fontSize: 14, margin: [10, 5]},
-    { text: "實際歸還日期：    月    日", fontSize: 14, margin: [10, 5]},
-    { text: "收件人員簽名：           ", fontSize: 14, margin: [10, 5]},
-    { text: "", margin: [10, 3] },    
-    { text: "【 器材借用表 Device Leasing List 】", fontSize: 16, margin: [10, 5] },
-    {
-      ul: [
-        <?php foreach($devices as $index => $device): ?>
-         { text: "<?php echo $device['name_zh-TW']."(".$device['name_en-us'].")" ?> 借用數目：<?php echo $device['lease_count']; ?>", fontSize: 14, margin: [20, 3] },
-        <?php endforeach; ?>
-      ]
-    },
+    { text: "・器材借用表", fontSize: 14, margin: [10, 5] },
+    table_b,
+    { text: "", margin: [10, 3] },
+    { text: '・借用申請核對資訊', fontSize: 14, margin: [10, 5] },
+    table_c
   ],
   defaultStyle: {
     font: 'kaiu'
